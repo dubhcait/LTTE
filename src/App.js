ace714dc0f7d3e5c1d4d8a63eeaa132f49757c2b
Kate O'Brien <kate.obrien@octoenergy.com>
2020-08-29 13:39:41 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,195 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <header className="App-header">Letters to the Internet</header>
+        {/* css grid  */}
+        <div className="letters">
+          <div className="letter l1"></div>
+          <div className="letter l2"></div>
+          <div className="letter l3"></div>
+          <div className="letter l4"></div>
+          <div className="letter l5"></div>
+          <div className="letter l6"></div>
+          <div className="letter l7"></div>
+          <div className="letter l8"></div>
+          <div className="letter l9"></div>
+          <div className="letter l10"></div>
+        </div>
+
+        <div class="set">
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+              height="100px"
+              width="100px"
+            />
+          </div>
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+              height="80px"
+              width="80px"
+            />
+          </div>
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+              height="80px"
+              width="80px"
+            />
+          </div>
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+              height="60px"
+              width="60px"
+            />
+          </div>
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+              height="60px"
+              width="60px"
+            />
+          </div>
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+              height="60px"
+              width="60px"
+            />
+          </div>
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+              height="80px"
+              width="80px"
+            />
+          </div>
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+              height="50px"
+              width="50px"
+            />
+          </div>
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+              height="50px"
+              width="50px"
+            />
+          </div>
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+              height="50px"
+              width="50px"
+            />
+          </div>
+
+          <div class="set set2">
+            <div>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+                height="50px"
+                width="120px"
+              />
+            </div>
+            <div>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+                height="50px"
+                width="120px"
+              />
+            </div>
+            <div>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+                height="50px"
+                width="120px"
+              />
+            </div>
+            <div>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+                height="50px"
+                width="120px"
+              />
+            </div>
+            <div>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+                height="50px"
+                width="120px"
+              />
+            </div>
+
+            <div class="set set3">
+              <div>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+                  height="100px"
+                  width="120px"
+                />
+              </div>
+              <div>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+                  height="100px"
+                  width="120px"
+                />
+              </div>
+              <div>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+                  height="100px"
+                  width="120px"
+                />
+              </div>
+              <div>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+                  height="100px"
+                  width="120px"
+                />
+              </div>
+              <div>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+                  height="100px"
+                  width="120px"
+                />
+              </div>
+              <div>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+                  height="100px"
+                  width="120px"
+                />
+              </div>
+              <div>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+                  height="100px"
+                  width="120px"
+                />
+              </div>
+              <div>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1024px-Square_-_black_simple.svg.png"
+                  height="100px"
+                  width="120px"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
